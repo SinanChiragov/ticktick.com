62cffcf764381bfd1919b248758b1d0644cb8561
SinanChiragov <99143813+SinanChiragov@users.noreply.github.com>
2022-02-10 05:38:43 +0300

    first-commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/future/Header';
+import Footer from '../components/future/Footer';
+
+function Home() {
+  return (
+    <div>
+          <Header />
+          <h1>This is the home page</h1>
+          <h1>This is the home page</h1>
+          <p>This page is the home page. This page is not don yet</p>
+          <Footer />
+    </div>
+  )
+}
+
+export default Home;
